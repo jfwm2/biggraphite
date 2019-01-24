d4284a6ffbcf05f0c0dbe1a764de7e88a90fb96b
Jean-Francois Weber-Marx <jf.webermarx@criteo.com>
2019-01-24 09:31:47 +0000

    create volumes directoris in build.sh
    Signed-off-by: Jean-Francois Weber-Marx <jf.webermarx@criteo.com>

--- docker/build.sh
+++ docker/build.sh
@@ -2,5 +2,8 @@
 
 for i in $(ls */Dockerfile | sed 's/\(.*\)\/.*/\1/');
 do
-  docker build ${i} -t ${i}
+ mkdir -p volumes_${i}  
+ docker build ${i} -t ${i}
 done
+
+cp ../biggraphite/drivers/cassandra.py volumes_bg-cassandra/
