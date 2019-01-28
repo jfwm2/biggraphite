a48f8b242f5e1b68e3ab5935d99ba16af9c9fc48
Jean-Francois Weber-Marx <jfwm@hotmail.com>
2019-01-28 15:06:25 +0000

    [wip] added kibana container
    Signed-off-by: Jean-Francois Weber-Marx <jfwm@hotmail.com>

--- docker/build.sh
+++ docker/build.sh
@@ -1,17 +1,26 @@
-#!/bin/bash
+#! /bin/bash
 
 AGG_CONF=volumes_graphite-aggregator-cache/conf
 WEB_CONF=volumes_graphite-web/conf
 
-for i in $(ls */Dockerfile | sed 's/\(.*\)\/.*/\1/');
-do
- mkdir -p volumes_${i}  
- docker build .. -t ${i} -f ${i}/Dockerfile
-done
+build_bg-cassandra (){
+  docker build bg-cassandra -t bg-cassandra -f bg-cassandra/Dockerfile
+}
 
-mkdir -p ${AGG_CONF} ${WEB_CONF}
+build_bg-elasticsearch (){
+  docker build bg-elasticsearch -t bg-elasticsearch -f bg-elasticsearch/Dockerfile
+}
 
-cat << EOF > ${AGG_CONF}/carbon.conf
+build_bg-kibana (){
+  docker build bg-kibana -t bg-kibana -f bg-kibana/Dockerfile
+}
+
+build_graphite-aggregator-cache (){
+  docker build .. -t graphite-aggregator-cache -f graphite-aggregator-cache/Dockerfile
+
+  mkdir -p ${AGG_CONF} volumes_graphite-aggregator-cache
+
+  cat << EOF > ${AGG_CONF}/carbon.conf
 [cache]
 BG_DATA_DRIVER = cassandra
 BG_METADATA_DRIVER = elasticsearch
@@ -26,9 +35,16 @@ BG_ELASTICSEARCH_PORT = 9200
 #BG_ELASTICSEARCH_INDEX_SUFFIX = _%Y-w%W
 EOF
 
-touch ${AGG_CONF}/storage-schemas.conf
+  touch ${AGG_CONF}/storage-schemas.conf
+}
 
-cat << EOF > ${WEB_CONF}/local_settings.py
+
+build_graphite-web (){
+  docker build .. -t graphite-web -f graphite-web/Dockerfile
+
+  mkdir -p ${AGG_WEB} volumes_graphite-web
+
+  cat << EOF > ${WEB_CONF}/local_settings.py
 DEBUG = True
 LOG_DIR = '/tmp'
 STORAGE_DIR = '/tmp'
@@ -46,7 +62,7 @@ BG_ELASTICSEARCH_HOSTS = 'elasticsearch'
 BG_ELASTICSEARCH_PORT = '9200'
 EOF
 
-cat << EOF > ${WEB_CONF}/launch-graphite-web.sh
+  cat << EOF > ${WEB_CONF}/launch-graphite-web.sh
 #! /bin/bash
 
 cp /conf/local_settings.py /usr/local/lib/python3.6/site-packages/graphite/
@@ -55,3 +71,46 @@ django-admin migrate
 django-admin migrate --run-syncdb
 cd /usr/local/lib/python3.6/site-packages/graphite && run-graphite-devel-server.py /usr/local
 EOF
+}
+
+build (){
+  case ${1} in
+    bg-cassandra)
+      build_bg-cassandra ;;
+    bg-elasticsearch)
+      build_bg-elasticsearch ;;
+    bg-kibana)
+      build_bg-kibana ;;
+    graphite-aggregator-cache)
+      build_graphite-aggregator-cache ;;
+    graphite-web)
+      build_graphite-web ;;
+    *)
+      usage ;;
+  esac
+}
+
+usage (){
+  echo "usage: $(basename ${0} \[dir1 \[dir2 ..\]\])"
+  exit 1
+}
+
+
+if [ -z "${1}" ]; then
+  for i in $(ls */Dockerfile | sed 's/\(.*\)\/.*/\1/');
+  do
+    TMP="${TMP} ${i}"
+  done
+else
+  while (( "$#" )); do
+    TMP="${TMP} ${1}"
+    shift
+  done
+fi
+
+# Remove lead whitespace
+LIST=$(echo ${TMP} | awk '$1=$1')
+
+for i in ${LIST}; do
+  build ${i}
+done
